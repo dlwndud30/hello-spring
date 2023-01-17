ef3077dea65380d2bb8e77422f1c177f58d71144
wnduddl30 <leejy3022@naver.com>
2023-01-17 23:24:36 +0900

    회원 서비스 테스트

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,65 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService = new MemberService(memberRepository);
+    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        memoryMemberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {  //한글로 바꿔도 OK
+
+        //given : 뭔가 또는 어떤 상황이 주어졌는데
+        Member member = new Member();
+        member.setName("hello");
+
+        //when : 언제 실행했더니
+        Long saveId = memberService.join(member);
+
+        //then : 결과가 이게 나와야함
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        //memberService.join(member2)을 실행할 때 IllegalStateException 예외가 터져야함
+        IllegalStateException e  = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원임");
+
+//        try{
+//            memberService.join(member2);
+//            fail();
+//        }catch (IllegalStateException e){
+//            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원임");
+//        }
+
+
+
+        //then
+    }
+
+    @Test
+    void findOne() {
+    }
+}
